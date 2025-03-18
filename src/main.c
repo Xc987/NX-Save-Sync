fd29176c5ba29bb15a2257c72d441a701e3a5121
Ricards Lacis <ricards.lacis18@gmail.com>
2025-03-18 20:18:58 +0200

    add template files

---
+++ src/main.c
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+
+#include <switch.h>
+
+int main(int argc, char **argv) {
+    consoleInit(NULL);
+    padConfigureInput(1, HidNpadStyleSet_NpadStandard);
+    PadState pad;
+    padInitializeDefault(&pad);
+    printf("\x1b[16;20HHello World!");
+    while(appletMainLoop()){
+        padUpdate(&pad);
+        u64 kDown = padGetButtonsDown(&pad);
+        if (kDown & HidNpadButton_Plus) {
+            break;
+        }
+        consoleUpdate(NULL);
+    }
+    consoleExit(NULL);
+    return 0;
+}
