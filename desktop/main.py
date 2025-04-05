7e023b7534ee10939034d425fbac12e62c21463e
Ricards Lacis <ricards.lacis18@gmail.com>
2025-04-05 20:50:49 +0300

    print current set switch ip and a button to set it / change it

--- desktop/main.py
+++ desktop/main.py
@@ -14,15 +14,16 @@ if getattr(sys, 'frozen', False):
     scriptDir = os.path.dirname(sys.executable)
 elif __file__:
     scriptDir = os.path.dirname(__file__)
-
+selected = 0
+selectedTitle = 0
 tempDir = os.path.join(scriptDir, "temp")
+zipPath = os.path.join(scriptDir, "temp.zip")
 input_text = None
 output_widget = None
 pressed_enter = False
 titles = []
 keys = []
 paths = []
-selected = 0
 server_thread = None
 
 def checkConfig():
@@ -33,11 +34,12 @@ def checkConfig():
     configDir.mkdir(exist_ok=True)
     configFile = configDir / 'config.json'
     if not configFile.exists():
-        print("\n\033[31m[FAIL] Config file doesnt exist!\033[0m")
-        
-    return configFile
-
+        return 0
+    else:
+        return configFile
+    
 def changeHost():
+    global pressed_enter
     appdataPath = os.getenv('LOCALAPPDATA')
     if not appdataPath:
         appdataPath = Path.home() / 'AppData' / 'Local'
@@ -46,18 +48,50 @@ def changeHost():
     configFile = configDir / 'config.json'
     if not configFile.exists():
         with open(configFile, 'w') as f:
-            print("\n\033[35m[LOAD]\033[0m Please input the Switch ip")
-            hostIp = input("\033[35m[LOAD]\033[0m > ")
+            with dpg.window(tag="input", label="Input Window", pos=(125, 125), no_resize=True, no_collapse=True, no_close=True, no_move=True, modal=True, width=300, height=100):
+                dpg.add_text("Input switch IP")
+                dpg.add_input_text(width=250, tag="input_widget")
+                input_entered = True
+                dpg.show_item("input_widget")
+                while input_entered:
+                    keyboard.on_press(checkInput)
+                    input_value = dpg.get_value("input_widget")
+                    if input_value != "" and pressed_enter:
+                        input_entered = True
+                        dpg.hide_item("input_widget")
+                        dpg.hide_item("input")
+                        input_entered = False
+                    time.sleep(0.1) 
+                keyboard.unhook_all()
+                pressed_enter = False
+                hostIp = input_value
             json.dump({"host": hostIp}, f, indent=4) 
     else:
         config = {}
         with open(configFile, 'r') as f:
             config = json.load(f)
-        print("\n\033[35m[LOAD]\033[0m Please input the Switch IP")
-        hostIp = input("\033[35m[LOAD]\033[0m > ")
+        with dpg.window(tag="input", label="Input Window", pos=(125, 125), no_resize=True, no_collapse=True, no_close=True, no_move=True, modal=True, width=300, height=100):
+            dpg.add_text("Input switch IP")
+            dpg.add_input_text(width=250, tag="input_widget")
+            input_entered = True
+            dpg.show_item("input_widget")
+            while input_entered:
+                keyboard.on_press(checkInput)
+                input_value = dpg.get_value("input_widget")
+                if input_value != "" and pressed_enter:
+                    input_entered = True
+                    dpg.hide_item("input_widget")
+                    dpg.hide_item("input")
+                    input_entered = False
+                time.sleep(0.1) 
+            keyboard.unhook_all()
+            pressed_enter = False
+        hostIp = input_value
         config["host"] = hostIp
         with open(configFile, 'w') as f:
             json.dump(config, f, indent=4)
+    dpg.set_value("current_ip", f"Switch IP: {hostIp}")
+    dpg.set_item_label("current_ip_button", "Change switch IP")
 
 def downloadZip(host, port, file_name):
     try:
@@ -90,7 +124,7 @@ def downloadZip(host, port, file_name):
                 header_complete = True
         if not content_length:
             printToWidget("Could not determine file size from headers\n")
-            return
+            return 0
         dpg.show_item("progress_bar")
         dpg.show_item("progress_info")
         while True:
@@ -109,7 +143,7 @@ def downloadZip(host, port, file_name):
         file_handle.close()
         if b"200 OK" not in header:
             printToWidget("Server error or file not found!\n")
-            return
+            return 0
         printToWidget(f"File {file_name} downloaded successfully.\n")
         dpg.hide_item("progress_bar")
         dpg.hide_item("progress_info")
@@ -118,8 +152,10 @@ def downloadZip(host, port, file_name):
         shutdown_socket.sendall(b"SHUTDOWN")
         shutdown_socket.close()
         printToWidget("Shutting down server.\n")
+        return 1
     except Exception as e:
         printToWidget(f"Error: {e}\n")
+        return 0
     finally:
         client_socket.close()
 
@@ -160,7 +196,6 @@ class uploadZip:
                 "Connection: close\r\n\r\n"
             )
             client_socket.send(headers.encode())
-            
             with open('temp.zip', 'rb') as f:
                 while True:
                     try:
@@ -169,21 +204,19 @@ class uploadZip:
                             break
                         client_socket.send(data)
                     except ConnectionError:
-                        break
-                        
+                        break        
         except Exception as e:
-            print(f"Client handling error: {str(e)}")
+            printToWidget2(f"Client handling error: {str(e)}\n")
         finally:
             try:
                 client_socket.close()
             except:
                 pass
-
     def run(self):
         self.server_socket.bind(('0.0.0.0', 8080))
         self.server_socket.listen(5)
         self.server_socket.settimeout(1)
-        print("\033[32m[ OK ]\033[0m Server is now running")
+        printToWidget2("Server is now running\n")
         try:
             while not self.shutdown_flag.is_set():
                 try:
@@ -193,16 +226,16 @@ class uploadZip:
                     continue
                 except OSError as e:
                     if not self.shutdown_flag.is_set():
-                        print(f"Accept error: {e}")
+                        printToWidget2(f"Accept error: {e}\n")
         finally:
+            printToWidget2("Shutting down server\n")
             self.server_socket.close()
-            print("\033[32m[ OK ]\033[0m Shutting down server")
             printToWidget2("Deleteing temp.zip file\n")
             os.remove(os.path.join(scriptDir, "temp.zip"))
             printToWidget2("Deleteing temp folder\n")
             shutil.rmtree(tempDir)
                
-def on_enter_press(event):
+def checkInput(event):
     global pressed_enter
     if event.name == 'enter':
         pressed_enter = True
@@ -211,11 +244,11 @@ def inputString():
     with dpg.window(tag="input", label="Input Window", pos=(125, 125), no_resize=True, no_collapse=True, no_close=True, no_move=True, modal=True, width=300, height=100):
         dpg.add_text("Input emulator save file path")
         dpg.add_input_text(width=250, tag="input_widget")
-    global input_value, input_entered, pressed_enter
+    global pressed_enter
     input_entered = True
     dpg.show_item("input_widget")
     while input_entered:
-        keyboard.on_press(on_enter_press)
+        keyboard.on_press(checkInput)
         input_value = dpg.get_value("input_widget")
         if input_value != "" and pressed_enter:
             input_entered = True
@@ -224,9 +257,10 @@ def inputString():
             input_entered = False
         time.sleep(0.1) 
     keyboard.unhook_all()
+    pressed_enter = False
     return input_value
 
-def window():
+def createWindow():
     global output_widget, output_widget2
     dpg.create_context()
     with dpg.font_registry():
@@ -252,7 +286,7 @@ def window():
             with dpg.tab(label="Pull"):
                 dpg.add_text("Pull current save file from switch to pc")
                 with dpg.group(horizontal=True):
-                    dpg.add_button(label="Connect to switch", width=150, height=30, callback=pull)
+                    dpg.add_button(label="Connect to switch", width=150, height=30, callback=startPull)
                     dpg.add_progress_bar(label="Temp", default_value=0, width=200, tag="progress_bar")
                     dpg.add_text("", tag="progress_info")
                     dpg.hide_item("progress_bar")
@@ -261,11 +295,21 @@ def window():
                 dpg.hide_item(output_widget)
             with dpg.tab(label="Push"):
                 dpg.add_text("Push newer save file from pc to switch")
-                dpg.add_button(label="Start server", width=150, height=30, callback=selectTitle)
+                dpg.add_button(label="Start server", width=150, height=30, callback=startPush)
                 output_widget2 = dpg.add_input_text(multiline=True, readonly=True, width=570, height=240, tab_input=True)
                 dpg.hide_item(output_widget2)
             with dpg.tab(label="Config"):
-                dpg.add_text("This is the content of Tab 3", indent=20)
+                configFile = checkConfig()
+                if configFile != 0:
+                    with open(configFile, 'r') as file:
+                        data = json.load(file)
+                        host = data.get("host")
+                        if host:
+                            dpg.add_text(f"Switch IP: {host}", tag="current_ip")
+                            dpg.add_button(label="Change switch IP", width=150, height=30, tag="current_ip_button", callback=changeHost)   
+                else:
+                    dpg.add_text("Switch IP not set!", tag="current_ip")
+                    dpg.add_button(label="Set switch IP", width=150, height=30, tag="current_ip_button", callback=changeHost)
     
     dpg.create_viewport(title='NX-Save-Sync', width=600, height=400, min_width=600, min_height=400, max_width=600, max_height=400)
     dpg.setup_dearpygui()
@@ -280,22 +324,29 @@ def window():
 def printToWidget(message):
     global output_widget
     if output_widget is not None:
-        dpg.set_value(output_widget, dpg.get_value(output_widget) + message)
+        dpg.set_value(output_widget, dpg.get_value(output_widget) + message, color=[255, 0, 0])
 
 def printToWidget2(message):
     global output_widget2
     if output_widget2 is not None:
         dpg.set_value(output_widget2, dpg.get_value(output_widget2) + message)
 
-def find_first_position(sender, app_data):
-    global selected
-    selected = titles.index(app_data)
-    dpg.hide_item("titles")
-    push()
+def getSelectedTitle(sender, app_data):
+    global selectedTitle
+    selectedTitle = titles.index(app_data)
+    dpg.delete_item("titles")
+    result = push()
+    if (result == 0):
+        printToWidget2("Process ended with an error!\n")
+    elif (result == 1):
+        printToWidget2("Process ended successfully!\n")
 
 def selectTitle():
     configFile = checkConfig()
-    print("\nSelect a title")
+    if configFile == 0:
+        dpg.show_item(output_widget2)
+        printToWidget2("Config file doesnt exist!\n")
+        return 0
     with open(configFile, 'r') as file:
         config = json.load(file)
     for key, value in config.items():
@@ -304,33 +355,56 @@ def selectTitle():
             paths.append(value[0])
             titles.append(value[1])
     if not titles:
-        print("\033[31m[FAIL] No valid entries found in config.json!\033[0m")
-        
-    
-    with dpg.window(tag="titles", label="Title selection Window", pos=(40, 50), no_resize=True, no_collapse=True, no_close=True, no_move=True, modal=True, width=500, height=275):
-        dpg.add_text("Select a title you want to push save file")
-        with dpg.child_window(border=False):
-            dpg.add_listbox(tag="array_listbox",items=titles,num_items=10,width=-1, callback=find_first_position)
+        dpg.show_item(output_widget2)
+        printToWidget2("No valid entries found in config.json!\n")
+        return 0
+    else:
+        with dpg.window(tag="titles", label="Title selection Window", pos=(40, 50), no_resize=True, no_collapse=True, no_close=True, no_move=True, modal=True, width=500, height=275):
+            dpg.add_text("Select a title you want to push save file")
+            with dpg.child_window(border=False):
+                dpg.add_listbox(tag="array_listbox",items=titles,num_items=10,width=-1, callback=getSelectedTitle)
+
+def startPush():
+    global selected
+    selected = 2
+    result = selectTitle()
+    if (result == 0):
+        printToWidget2("Process ended with an error!\n")
+    elif (result == 1):
+        printToWidget2("Process ended successfully!\n")
+
+def startPull():
+    global selected
+    selected = 1
+    result = pull()
+    if (result == 0):
+        printToWidget("Process ended with an error!\n")
+    elif (result == 1):
+        printToWidget("Process ended successfully!\n")
 
 def pull():
     global output_widget
     dpg.set_value(output_widget, "")
     dpg.show_item(output_widget)
     configFile = checkConfig()
+    if configFile == 0:
+        printToWidget("Config file doesnt exist!\n")
+        return 0
     with open(configFile, 'r') as file:
         data = json.load(file)
         host = data.get("host")
         if host:
             printToWidget(f"Connecting to host: {host}\n")
-    downloadZip(host, 8080, "temp.zip")
+    if downloadZip(host, 8080, "temp.zip") == 0:
+        return 0
     if os.path.exists(os.path.join(scriptDir, "temp.zip")):
         printToWidget("Unzipping temp.zip\n")
     else:
         printToWidget("Couldnt find temp.zip!\n")
-    zip_path = os.path.join(scriptDir, "temp.zip")
-    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        return 0
+    
+    with zipfile.ZipFile(zipPath, 'r') as zip_ref:
         zip_ref.extractall(scriptDir)
-    tempDir = os.path.join(scriptDir, "temp")
     if os.path.exists(tempDir) and os.path.isdir(tempDir):
         subFolder = [f.name for f in os.scandir(tempDir) if f.is_dir()]
         if len(subFolder) == 1:
@@ -353,10 +427,10 @@ def pull():
                         json.dump(data, file, indent=4)
         else:
             printToWidget("Couldnt find any TID subfolders in /temp/!\n")
-            
+            return 0
     else:
         printToWidget("Couldnt find temp folder!\n")
-        
+        return 0
     with open(configFile, 'r') as file:
         data = json.load(file)
         titleArray = data[subFolder[0]]
@@ -366,7 +440,7 @@ def pull():
         printToWidget(f"Deleting any existing save file in {dstDir}\n")
     else:
         printToWidget(f"The directory {dstDir} does not exist!\n")
-        
+        return 0
     for item in os.listdir(dstDir):
         itemPath = os.path.join(dstDir, item)
         try:
@@ -376,6 +450,7 @@ def pull():
                 shutil.rmtree(itemPath)
         except Exception as e:
             printToWidget(f"Failed to delete {itemPath}!\n")
+            return 0
     printToWidget("Moving save file.\n")
     for item in os.listdir(srcDir):
         srcItem = os.path.join(srcDir, item)
@@ -388,21 +463,24 @@ def pull():
     os.remove(os.path.join(scriptDir, "temp.zip"))
     printToWidget("Deleteing temp folder.\n")
     shutil.rmtree(tempDir)
+    return 1
 
 def push():
+    dpg.set_value(output_widget2, "")
     dpg.show_item(output_widget2)
-    printToWidget2(f"Selected title: {titles[selected]}\n")
-    printToWidget2(f"Selected TID: {keys[selected]}\n")
-    printToWidget2(f"Title save data path: {paths[selected]}\n")
-    
+    printToWidget2(f"Selected title: {titles[selectedTitle]}\n")
+    printToWidget2(f"Selected TID: {keys[selectedTitle]}\n")
+    printToWidget2(f"Title save data path: {paths[selectedTitle]}\n")
     os.mkdir(tempDir)
-    subFolder = os.path.join(tempDir, keys[selected])
+    subFolder = os.path.join(tempDir, keys[selectedTitle])
     os.mkdir(subFolder)
     printToWidget2("Exporting save data\n")
-    if os.path.exists(paths[selected]) and os.path.isdir(paths[selected]):
-        shutil.copytree(paths[selected], subFolder, dirs_exist_ok=True)
+    if os.path.exists(paths[selectedTitle]) and os.path.isdir(paths[selectedTitle]):
+        shutil.copytree(paths[selectedTitle], subFolder, dirs_exist_ok=True)
     else:
         printToWidget2("Couldnt find the save data folder!\n")
+        shutil.rmtree("temp")
+        return 0
     with zipfile.ZipFile('temp.zip', 'w', zipfile.ZIP_DEFLATED) as zipf:
         for root, dirs, files in os.walk('temp'):
             for file in files:
@@ -417,4 +495,4 @@ def push():
     server_thread.start()
 
 if __name__ == "__main__":
-    window()
+    createWindow()
